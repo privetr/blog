9e59e80cbc6e7aa3742ff379efac0a83cf1885a4
Remi Privet <privet.remi@gmail.com>
2015-03-16 18:19:40 +0100

    Initialize repository

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,28 @@
+class CommentsController < ApplicationController
+
+	# AUTHENTICATION 
+	http_basic_authenticate_with name: "remi", password: "remi", only: :destroy
+
+	# PUBLIC methods 
+
+	def create
+		@article = Article.find(params[:article_id])
+	    @comment = @article.comments.create(comment_params)
+	    redirect_to article_path(@article)
+	end
+ 
+
+ 	def destroy
+	    @article = Article.find(params[:article_id])
+	    @comment = @article.comments.find(params[:id])
+	    @comment.destroy
+	    redirect_to article_path(@article)
+  	end
+
+ 	# PRIVATE methods 
+
+	private
+		def comment_params
+			params.require(:comment).permit(:commenter, :body)
+		end
+end
